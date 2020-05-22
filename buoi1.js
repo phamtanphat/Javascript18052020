09b995a9789465d6d5dc464aedcc19635bdc8e2a
phamtanphat <phatdroid94@gmail.com>
2020-05-22 03:59:50 -0700

    kieu du lieu

---
+++ buoi1.js
@@ -0,0 +1,18 @@
+// 1 : Khai bao bien
+// var a = "Nguyen Van Teo"
+// 2 : Kieu du lieu
+// Primities type (Kieu nguyen thuy)
+//  + Null
+// var a = null
+// console.log(a)
+// + Undefined
+// TH1 : Khai bao bien nhung khong co gia tri
+// var a
+// console.log(a)
+// Object type (Kieu doi tuong)
+// JSON
+// var teo = {name : 'Nguyen Van Teo', age : 20}
+// console.log(typeof teo)
+// Array type
+var arrayName = ["Teo","Ti","Tun"]
+console.log(arrayName)
