318518084db0fd57edec9151642580c3aca3bbd9
phamtanphat <phatdroid94@gmail.com>
2020-05-27 04:36:14 -0700

    bai tap reduce

--- buoi3.js
+++ buoi3.js
@@ -52,27 +52,57 @@
     { id: 3, name: 'Nội thất'}
   ];
 */
-var listProducts = [
- {id: 1, categoryId: 1, name: 'Tivi'},
- {id: 2, categoryId: 1, name: 'Tủ lạnh'},
- {id: 3, categoryId: 3, name: 'Ghế sofa'},
- {id: 4, categoryId: 1, name: 'Máy giặt'},
- {id: 5, categoryId: 2, name: 'Chén bát'},
- {id: 6, categoryId: 2, name: 'Nồi cơm điện'},
- {id: 7, categoryId: 3, name: 'Cửa kính'},
- {id: 8, categoryId: 1, name: 'Điều hoà'},
- {id: 9, categoryId: 3, name: 'Bàn tròn'},
- {id: 10, categoryId: 2, name: 'Lò vi sóng'},
-]
+// var listProducts = [
+//  {id: 1, categoryId: 1, name: 'Tivi'},
+//  {id: 2, categoryId: 1, name: 'Tủ lạnh'},
+//  {id: 3, categoryId: 3, name: 'Ghế sofa'},
+//  {id: 4, categoryId: 1, name: 'Máy giặt'},
+//  {id: 5, categoryId: 2, name: 'Chén bát'},
+//  {id: 6, categoryId: 2, name: 'Nồi cơm điện'},
+//  {id: 7, categoryId: 3, name: 'Cửa kính'},
+//  {id: 8, categoryId: 1, name: 'Điều hoà'},
+//  {id: 9, categoryId: 3, name: 'Bàn tròn'},
+//  {id: 10, categoryId: 2, name: 'Lò vi sóng'},
+// ]
 
-function filterProductsByCategoryId(products, categoryId) {
-  // your code here!
-  return products.filter(function(value){
-      return value.categoryId == categoryId
-  }).map(function(value){
-      return {id : value.id , name : value.name}
-  })
+// function filterProductsByCategoryId(products, categoryId) {
+//   // your code here!
+//   return products.filter(function(value){
+//       return value.categoryId == categoryId
+//   }).map(function(value){
+//       return {id : value.id , name : value.name}
+//   })
+// }
+
+// console.log(filterProductsByCategoryId(listProducts,2))
+
+
+// 14 : Reduce
+var numArr = [1,2,3,4,5,6,7,8,9,10]
+
+// Su dung reduce khong co gia tri khoi tao
+// var total = numArr.reduce(function(prevalue , currentValue ){
+//     console.log(prevalue , currentValue)
+//     return prevalue + currentValue
+// })
+
+// Trả về số lượng người đã vote
+function totalVotes(arr) {
+
 }
 
-console.log(filterProductsByCategoryId(listProducts,2))
-
+var voters = [
+  {name:'Bob' , age: 30, voted: true},
+  {name:'Jake' , age: 32, voted: true},
+  {name:'Kate' , age: 25, voted: false},
+  {name:'Sam' , age: 20, voted: false},
+  {name:'Phil' , age: 21, voted: true},
+  {name:'Ed' , age:55, voted:true},
+  {name:'Tami' , age: 54, voted:true},
+  {name: 'Mary', age: 31, voted: false},
+  {name: 'Becky', age: 43, voted: false},
+  {name: 'Joey', age: 41, voted: true},
+  {name: 'Jeff', age: 30, voted: true},
+  {name: 'Zack', age: 19, voted: false}
+];
+totalVotes(voters) // 7
