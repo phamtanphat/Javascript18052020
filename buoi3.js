7cb9d0f27e82c8aa4de5b622a30214e031914ee5
phamtanphat <phatdroid94@gmail.com>
2020-05-27 04:19:01 -0700

    bai tap 1 : filter

--- buoi3.js
+++ buoi3.js
@@ -16,21 +16,50 @@
 
 // console.log(myDog1.showWeight == myDog2.showWeight)
 
-var numArray = [5,10,1,8,9]
+// var numArray = [5,10,1,8,9]
 
 // var newNumArray = numArray.map(function(value , index){
 //     return value * 2
 // })
 // console.log(newNumArray)
 
-Array.prototype.mymap = function(cb){
-    var newArr = []
-    for(var i = 0 ; i < this.length ; i++){
-        newArr.push(cb(this[i], i))   
+// Array.prototype.mymap = function(cb){
+//     var newArr = []
+//     for(var i = 0 ; i < this.length ; i++){
+//         newArr.push(cb(this[i], i))   
+//     }
+//     return newArr
+// }
+// var newNumArray = numArray.mymap(function(value , index){
+//     return value * 2
+// })
+// console.log(newNumArray)
+
+// 13 : Filter
+// var numArr = [1,2,3,4,5,6,7,8,9,10]
+// var newArr = numArr.filter(function(value , index){
+//     return value % 2 == 0
+// })
+// console.log(newArr)
+var arrUsers = [
+    {
+      name: "Angelina Jolie",
+      age: 80
+    },
+    {
+      name: "Eric Jones",
+      age: 2
+    },
+    {
+      name: "Paris Hilton",
+      age: 5
+    },
+    {
+      name: "Kayne West",
+      age: 16
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100
     }
-    return newArr
-}
-var newNumArray = numArray.mymap(function(value , index){
-    return value * 2
-})
-console.log(newNumArray)
+  ]
