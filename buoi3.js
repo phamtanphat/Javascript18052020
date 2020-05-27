c21a699da40f5e1f1530c0ceaa45398852b16d3b
phamtanphat <phatdroid94@gmail.com>
2020-05-27 04:26:43 -0700

    bai tap 1 filter

--- buoi3.js
+++ buoi3.js
@@ -41,25 +41,38 @@
 //     return value % 2 == 0
 // })
 // console.log(newArr)
-var arrUsers = [
-    {
-      name: "Angelina Jolie",
-      age: 80
-    },
-    {
-      name: "Eric Jones",
-      age: 2
-    },
-    {
-      name: "Paris Hilton",
-      age: 5
-    },
-    {
-      name: "Kayne West",
-      age: 16
-    },
-    {
-      name: "Bob Ziroll",
-      age: 100
-    }
-  ]
+// Ví dụ
+/* Filter các sản phẩm khi biết id danh mục sản phẩm
+  Ví dụ đây là 1 danh sách các danh mục sản phẩm, 
+  khi người dùng muốn lọc các sản phẩm thuộc danh mục nào 
+  thì filter các sản phẩm của danh mục đó.
+  var categories = [
+    { id: 1, name: 'Đồ điện tử'},
+    { id: 2, name: 'Đồ gia dụng'},
+    { id: 3, name: 'Nội thất'}
+  ];
+*/
+var listProducts = [
+ {id: 1, categoryId: 1, name: 'Tivi'},
+ {id: 2, categoryId: 1, name: 'Tủ lạnh'},
+ {id: 3, categoryId: 3, name: 'Ghế sofa'},
+ {id: 4, categoryId: 1, name: 'Máy giặt'},
+ {id: 5, categoryId: 2, name: 'Chén bát'},
+ {id: 6, categoryId: 2, name: 'Nồi cơm điện'},
+ {id: 7, categoryId: 3, name: 'Cửa kính'},
+ {id: 8, categoryId: 1, name: 'Điều hoà'},
+ {id: 9, categoryId: 3, name: 'Bàn tròn'},
+ {id: 10, categoryId: 2, name: 'Lò vi sóng'},
+]
+
+function filterProductsByCategoryId(products, categoryId) {
+  // your code here!
+  return products.filter(function(value){
+      return value.categoryId == categoryId
+  }).map(function(value){
+      return {id : value.id , name : value.name}
+  })
+}
+
+console.log(filterProductsByCategoryId(listProducts,2))
+
