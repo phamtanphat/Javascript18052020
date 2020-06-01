611af664b3d7fbc86d5b9ca1943b6b42af6e4a38
phamtanphat <phatdroid94@gmail.com>
2020-06-01 04:09:42 -0700

    vi du function context

--- buoi4.js
+++ buoi4.js
@@ -1,11 +1,25 @@
-const mouse = {
-    name : 'Mickey',
-    sayhi : function() {
-      console.log('Hi, my name is' , this.name)
-    }
-}
+// const mouse = {
+//     name : 'Mickey',
+//     sayhi : function() {
+//       console.log('Hi, my name is' , this.name)
+//     }
+// }
 // mouse.sayhi()
 // console.log(this)
 
-const sayHello = mouse.sayhi
-sayHello.bind({name : 'Cat'})()
+// const sayHello = mouse.sayhi
+// sayHello.bind({name : 'Cat'})()
+function run(cb){
+    console.log('run...')
+    cb()
+}
+  
+const mouse = {
+    name : 'Mickey',
+    run : function() {
+        console.log(this.name + ' is running')
+    }
+}
+  
+run(mouse.run.bind(mouse))
+  
