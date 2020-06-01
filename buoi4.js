e76e4c15347421c11f8a8d1512d7566fdcc2195e
phamtanphat <phatdroid94@gmail.com>
2020-06-01 05:22:53 -0700

    enhanced object literals

--- buoi4.js
+++ buoi4.js
@@ -95,3 +95,30 @@
 // }
 // console.log(showName('phat'))
 
+// 23 : rest
+// function sum(...nums){
+//     return nums.reduce((a ,b) => a + b)
+//   }
+// sum(1,2,3,4,5)
+
+// 24 : spread
+// const a = [1,2,3]
+
+// function sum(...numbers){
+//     console.log(numbers.join())
+// }
+// sum(...a)
+
+// const obj1 = {
+//     a : 1,
+//     b : 2,
+//     c : 3,
+//     d : {e : 10}
+//   }
+// let obj2 = {}
+// for(let key in obj1){
+//   obj2[key] = obj1[key]
+// }
+// let obj2 = {...obj1}
+
+// console.log(obj2)
